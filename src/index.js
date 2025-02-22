65210c518b32e3c1e4ac276712061e022a0369fb
Roche Ngaleu <ibikivan1@gmail.com>
2025-02-22 01:03:16 +0100

    Complétion de l'API; seules restes à ajouter les entêtes spécialisée, la gestions CORS et le https

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const { syncDatabase, testDbConnexion } = require('./models');
 const userRoutes = require('./routes/userRoutes');
 const egopassRoutes = require('./routes/egopassRoutes');
 const travelersRoutes = require('./routes/travelersRoutes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./swagger');
 
 const app = express();
 testDbConnexion();
@@ -19,6 +21,9 @@ app.use('/api/egopass', egopassRoutes);
 
 // Travels routes
 app.use('/api/travels', travelersRoutes);
+
+// Swagger UI
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Middleware de gestion des erreurs pour les routes et middlewares d'Express
 app.use(errorHandler);
---
+++ src/swagger.js
@@ -0,0 +1,37 @@
+const swaggerJSDoc = require('swagger-jsdoc');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'e-GoPass API',
+      version: '1.0.0',
+      description: 'Documentation de l’API e-GoPass',
+    },
+    servers: [
+      {
+        url: `http://localhost:${process.env.PORT}`,
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+    security: [{
+      bearerAuth: []
+    }],
+  },
+  apis: ['./src/routes/*.js', './src/controllers/*.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+module.exports = swaggerSpec;
